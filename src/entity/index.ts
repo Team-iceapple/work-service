780e82ae3234fcc916c1b352c2d80be4acce2e59
R3pic <101osc@naver.com>
2025-05-23 22:26:10 +0900

    feat: Entity 정의

---
+++ src/entity/index.ts
@@ -0,0 +1,1 @@
+export { WorkEntity } from './work.entity';
---
+++ src/entity/work.entity.ts
@@ -0,0 +1,10 @@
+export class WorkEntity {
+    id: string;
+    name: string;
+    members: string;
+    thumbnail: string;
+    pdf_url: string;
+    description: string;
+    year: number;
+    created_at: Date;
+}
