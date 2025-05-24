20109f599e017183adc28810eaedc3820bceed18
R3pic <101osc@naver.com>
2025-05-24 14:29:08 +0900

    feat: Validation 추가

--- src/dto/create-work.body.ts
+++ src/dto/create-work.body.ts
@@ -1,6 +1,16 @@
+import {IsNumber, IsString, Length} from 'class-validator';
+
 export class CreateWorkBody {
+    @IsString()
+    @Length(1, 255)
     name: string;
+
+    @IsString({ each: true })
     members: string[];
+
+    @IsString()
     description: string;
+
+    @IsNumber()
     year: number;
 }
